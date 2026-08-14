b176f79ce905b1f054d27542329dfdc6b1c0df46
Alban Rauch <albanrauch@protonmail.com>
2026-08-14 21:42:53 +0200

    Added some test for the slurm.

---
+++ test/slurm/test_slurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+################################################################################################
+#SBATCH -J speed_test               # Job name               (or --job-name=quantum_sim)
+#SBATCH -p MI350P_600W              # Partition name         (or --partition=0745-1R5600-NOIB)
+#SBATCH -N 1                        # Nodes requested        (or --nodes=1)
+#SBATCH --exclusive                 # Resource not shared with other users
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --time=00:05:00             # Time limit
+#SBATCH --output=outputs/%x_%j.out  # Output file
+#SBATCH --error=outputs/%x_%j.err   # Error file
+################################################################################################
+
+module purge
+module load pennylane-amdgpu/0.45.0-rocm7.1.1-gfx950
+
+cd ~/test
+
+srun python -c "print('Hello')"
